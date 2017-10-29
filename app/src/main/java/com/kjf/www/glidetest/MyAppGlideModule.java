3cc477a4753597c3a5d185d12308b189f20176e3
Kesion-X <673080885@qq.com>
2017-10-29 21:52:20 +0800

    android studio plugin更新到3.0 gradle 更新到4.1 否则无法在build.gradle中增加 annotationProcessor 'com.github.bumptech.glide:compiler:4.2.0', 因为 annotationProcessor在Android studio 2.1.1 gradle 2.1之前，没有此方法

---
+++ app/src/main/java/com/kjf/www/glidetest/MyAppGlideModule.java
@@ -0,0 +1,11 @@
+package com.kjf.www.glidetest;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * Created by Administrator on 2017/10/29.
+ */
+@GlideModule
+public class MyAppGlideModule extends AppGlideModule{
+}
